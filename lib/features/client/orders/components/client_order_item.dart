86aae916f9ec64901d9c4da4d6e3a86390165bbd
Eng.Abdu khalid yahya Al_hrazi <82823586+almomyz@users.noreply.github.com>
2025-05-21 21:09:18 +0300

    style(client_order): update colors for improved readability
    Changed various text colors in the ClientOrderCard component to black for better contrast and readability. Adjusted date formatting to use a fixed locale, enhancing consistency across different user settings.

--- lib/features/client/orders/components/client_order_item.dart
+++ lib/features/client/orders/components/client_order_item.dart
@@ -28,7 +28,7 @@ class ClientOrderCard extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 15.h),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8.r),
-          border: Border.all(color: context.borderColor),
+          border: Border.all(color: Colors.black),
           color: context.hoverColor,
         ),
         child: Column(
@@ -38,23 +38,13 @@ class ClientOrderCard extends StatelessWidget {
             Row(
               mainAxisSize: MainAxisSize.max,
               children: [
-                CustomRadiusIcon(
-                  size: 40.h,
-                  backgroundColor: context.primaryColorLight,
-                  child: CustomImage(
-                    data.agent.id != '' ? data.agent.image : Assets.images.logo.path,
-                    height: 36.h,
-                    width: 36.h,
-                    borderRadius: BorderRadius.circular(40.h),
-                  ),
-                ).withPadding(end: 10.w),
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         data.agent.id != '' ? data.agent.fullname : LocaleKeys.no_agent.tr(),
-                        style: context.mediumText.copyWith(fontSize: 14),
+                        style: context.mediumText.copyWith(fontSize: 14, color: Colors.black),
                       ),
                       SizedBox(height: 8.h),
                       Row(
@@ -63,11 +53,12 @@ class ClientOrderCard extends StatelessWidget {
                             Assets.svg.location,
                             height: 16.sp,
                             width: 16.sp,
+                            color: Colors.black,
                           ).withPadding(end: 4.w),
                           Expanded(
                             child: Text(
                               data.address.placeDescription,
-                              style: context.regularText.copyWith(fontSize: 12, color: context.hintColor),
+                              style: context.regularText.copyWith(fontSize: 12, color: Colors.black),
                             ),
                           ),
                         ],
@@ -84,11 +75,11 @@ class ClientOrderCard extends StatelessWidget {
             Text.rich(
               TextSpan(
                 text: LocaleKeys.the_service.tr(),
-                style: context.regularText.copyWith(color: context.hintColor),
+                style: context.regularText.copyWith(color: Colors.black),
                 children: [
                   TextSpan(
                     text: data.typeTrans,
-                    style: context.boldText,
+                    style: context.boldText.copyWith(color: Colors.black),
                   ),
                 ],
               ),
@@ -99,26 +90,26 @@ class ClientOrderCard extends StatelessWidget {
                   Assets.svg.calender,
                   height: 16.sp,
                   width: 16.sp,
-                  color: context.hintColor,
+                  color: Colors.black,
                 ).withPadding(end: 5.w),
                 Text(
-                  DateFormat('dd MMM yyyy', context.locale.languageCode).format(data.createdAt),
+                  DateFormat('dd MMM yyyy', 'en').format(data.createdAt),
                   style: context.regularText.copyWith(
                     fontSize: 12,
-                    color: context.hintColor,
+                    color: Colors.black,
                   ),
                 ),
                 CustomImage(
                   Assets.svg.clock,
                   height: 16.sp,
                   width: 16.sp,
-                  color: context.hintColor,
+                  color: Colors.black,
                 ).withPadding(start: 12.w, end: 5.w),
                 Text(
-                  DateFormat('hh:mm a', context.locale.languageCode).format(data.createdAt),
+                  DateFormat('hh:mm a', 'en').format(data.createdAt),
                   style: context.regularText.copyWith(
                     fontSize: 12,
-                    color: context.hintColor,
+                    color: Colors.black,
                   ),
                 ),
               ],
